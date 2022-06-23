a6d17c583d9234e690a6a216e2ecb5cccb27e3fa
reallupin <kuduy1607@gmail.com>
2022-06-23 10:04:28 +0700

    merger

--- eCommerceProject/Controllers/HomeController.cs
+++ eCommerceProject/Controllers/HomeController.cs
@@ -1,9 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using eCommerceProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace eCommerceProject.Controllers
 {
 	public class HomeController : Controller
 	{
+		private ApplicationUser _user;
+		private ApplicationDbContext _context;
+		private UserManager<ApplicationUser> _usermanager;
+		private ApplicationUserManager _userManager;
+		private ApplicationDbContext db = new ApplicationDbContext();
+		public HomeController()
+		{
+			_user = new ApplicationUser();
+			_context = new ApplicationDbContext();
+			_usermanager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+		}
 		public ActionResult Index()
 		{
 			return View();
@@ -21,6 +36,11 @@ namespace eCommerceProject.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+		public ActionResult BannerSlider()
+		{
+			var banner = _context.BannerSliders.ToList();
+			return View(banner);
 		}
 		public ActionResult ProductDetail()
 		{
