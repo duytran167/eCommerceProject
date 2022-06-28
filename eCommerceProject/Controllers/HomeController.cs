b641e834a01983e9791e6f982231bdb2f8af2a9e
reallupin <kuduy1607@gmail.com>
2022-06-28 11:42:44 +0700

    validation register and login

--- eCommerceProject/Controllers/HomeController.cs
+++ eCommerceProject/Controllers/HomeController.cs
@@ -1,9 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using eCommerceProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace eCommerceProject.Controllers
 {
 	public class HomeController : Controller
 	{
+		private ApplicationUser _user;
+		private ApplicationDbContext _context;
+		private UserManager<ApplicationUser> _usermanager;
+		private ApplicationUserManager _userManager;
+		private ApplicationDbContext db = new ApplicationDbContext();
+		public HomeController()
+		{
+			_user = new ApplicationUser();
+			_context = new ApplicationDbContext();
+			_usermanager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+		}
 		public ActionResult Index()
 		{
 			return View();
@@ -26,12 +41,17 @@ namespace eCommerceProject.Controllers
 		{
 			return View();
 		}
-
+		public ActionResult BannerSlider()
+		{
+			var banner = _context.BannerSliders.ToList();
+			return View(banner);
+		}
 		public ActionResult _footer()
 		{
 
 
 			return View();
 		}
+
 	}
 }
